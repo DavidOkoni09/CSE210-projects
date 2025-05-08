b0fdef7c038358f6c03afed027ee1827502be369
Your Name <vee2004.david@gmail.com>
2025-05-08 16:43:59 +0100

    Made a number guessing game

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -2,8 +2,54 @@ using System;
 
 class Program
 {
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 100);
+
+        Console.WriteLine("Guess a number! What is your guess? ");
+        string guess = Console.ReadLine();
+        int numberGuess = int.Parse(guess);
+
+        if (number == numberGuess)
+        {
+            Console.WriteLine("Congratulations!!! You guessed it.");
+        }
+        else if (!(number == numberGuess))
+        {
+            if (number > numberGuess)
+                {
+                    Console.WriteLine("Guess higher.");
+                }
+                else
+                {
+                    Console.WriteLine("Guess lower.");
+                }
+
+            while (!(number == numberGuess))
+            {
+                Console.WriteLine("What is your guess? ");
+                guess = Console.ReadLine();
+                numberGuess = int.Parse(guess);
+
+                if (number > numberGuess)
+                {
+                    Console.WriteLine("Guess higher.");
+                }
+                else if (number == numberGuess)
+                {
+                    Console.WriteLine("Congratulations!!! You guessed it.");
+                }
+                else if (number < numberGuess)
+                {
+                    Console.WriteLine("Guess lower.");
+                }
+            }
+            
+        }
+
+        
+        
     }
 }
