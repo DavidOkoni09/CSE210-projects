6ca9fe8d3c85a09882c499faa6af5bc90b4e45a4
Your Name <vee2004.david@gmail.com>
2025-05-10 22:53:44 +0100

    Made a list work program

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,42 @@
 using System;
+using System.Collections.Generic;
+//using System.Linq;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        int digitsNew;
+        int largest = 0;
+
+        do
+        {
+            Console.Write("Enter number: ");
+            string digits = Console.ReadLine();
+            digitsNew = int.Parse(digits);
+            numbers.Add(digitsNew);
+
+        }  while (digitsNew != 0);
+        
+           numbers.RemoveAt(numbers.Count - 1);
+           int sum = numbers.Sum();
+           Console.WriteLine($"The sum is: {sum}");
+           double average = numbers.Average();
+           Console.WriteLine($"The average is: {average}");
+
+           foreach (int number in numbers)
+           {
+                if (number > largest)
+                {
+                    largest = number;
+                }
+           }
+           Console.WriteLine($"The largest number is: {largest}");
+        
+
+
     }
 }
