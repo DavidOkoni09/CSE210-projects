e315ce487eaadaada23de72af8433133bbc05282
Your Name <vee2004.david@gmail.com>
2025-05-13 15:02:13 +0100

    Corrected the job class

--- week02/Resumes/Job.cs
+++ week02/Resumes/Job.cs
@@ -1,10 +1,4 @@
-Job job = new Job();
-job._companyName = "Microsoft";
-job._jobTitle = "Data Analyst";
-job._startYear = 2022;
-job._endYear = 2025;
-job.ShowResult();
-
+using System;
 public class Job
 {
     // Keeps track of the company, job title, start year, and end year.
@@ -12,13 +6,4 @@ public class Job
     public string _jobTitle = "";
     public int _startYear;
     public int _endYear;
-
-    public Job()
-    {
-    }
-
-    public void ShowResult()
-    {
-        Console.WriteLine($"{_jobTitle} ({_companyName}) {_startYear}-{_endYear}.");
-    }
 }
--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -5,5 +5,17 @@ partial class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Resumes Project.");
+
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._companyName = "Microsoft";
+        job1._startYear = 2018;
+        job1._endYear = 2022;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Editor";
+        
+
+        Console.WriteLine(job1._companyName);
     }
 }
