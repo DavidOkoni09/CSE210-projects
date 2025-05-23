1ed7e66801615ce4db9c5565f9cb681576a91025
Your Name <vee2004.david@gmail.com>
2025-05-23 13:37:12 +0100

    Completed the scripture memorizer program

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -1,9 +1,33 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the ScriptureMemorizer Project.");
+
+        Reference reference = new Reference("Proverbs", 3, 5, 6);
+        Scripture scripture = new Scripture(reference, "Trust in the Lord with all thine heart: and lean not unto thine own understanding");
+
+        while (!scripture.IsCompletelyHidden())
+        {
+            Console.Clear();
+            Console.WriteLine(scripture.GetDisplayText());
+            Console.WriteLine("Press the enter key to start and continue or type quit to end. ");
+            string input = Console.ReadLine();
+
+            if (input.ToLower() == "quit")
+                break;
+            
+            scripture.HideRandomWords(3);
+        }
+
+        Console.Clear();
+        Console.WriteLine(scripture.GetDisplayText());
+        Console.WriteLine("All words hidden.");
+        
     }
 }
---
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -0,0 +1,37 @@
+using System;
+
+public class Scripture
+{
+    private Reference _reference;
+    private List<Word> _words;
+
+    public Scripture(Reference Reference, string text)
+    {
+        _reference = Reference;
+        _words = text.Split(" ").Select(word => new Word(word)).ToList();
+    }
+
+    public void HideRandomWords(int numberToHide)
+    {
+        Random rand = new Random();
+        List<Word> visibleWords = _words.Where(w => !w.IsHidden()).ToList();
+
+        for (int i = 0; i < numberToHide && visibleWords.Count > 0; i++)
+        {
+            int index = rand.Next(visibleWords.Count);
+            visibleWords[index].Hide();
+            visibleWords.RemoveAt(index);
+        }
+    }
+
+    public string GetDisplayText()
+    {
+
+        return _reference.GetDisplayText() + " " + string.Join(" ", _words.Select(w => w.GetDisplayText()));
+    }
+
+    public bool IsCompletelyHidden()
+    {
+        return _words.All(w => w.IsHidden());
+    }
+}
