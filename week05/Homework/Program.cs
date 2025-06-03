25f7b9ab288dc73ab9b3046719f21ffc27c94cb5
Your Name <vee2004.david@gmail.com>
2025-06-03 16:16:18 +0100

    Completed the homework code

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        MathAssignment m1 = new MathAssignment("Samuel Bennett", "Multiplication", "Section 7.3", "Problems 8-19");
+        Console.WriteLine(m1.GetSummary());
+        Console.WriteLine(m1.GetHomeworkList());
+
+        WritingAssignment w1 = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(w1.GetSummary());
+        Console.WriteLine(w1.GetWritingInformation());
     }
 }
